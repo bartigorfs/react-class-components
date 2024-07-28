73113422a01eb65a75b2eb00f92dea56b6e1169c
Igor Bartashevich <86968311+bartigorfs@users.noreply.github.com>
2024-07-28 16:18:45 +0300

    fix: testing setup feat: header tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { configDefaults } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
