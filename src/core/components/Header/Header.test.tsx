7c41cdddf93cda29d02ea8825c8461f22957cf02
Igor Bartashevich <86968311+bartigorfs@users.noreply.github.com>
2024-07-28 16:18:55 +0300

    fix: testing setup feat: header tests

--- src/core/components/Header/Header.test.tsx
+++ src/core/components/Header/Header.test.tsx
@@ -1,38 +1,38 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import { describe, it, expect, vi } from 'vitest';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { describe, it, expect, vi } from 'vitest'
 import Header from '@components/Header/Header.tsx'
 
 vi.mock('./Header.module.css', () => ({
   __esModule: true,
   default: {
     header: 'mocked-header-class',
-  }
-}));
+  },
+}))
 
 describe('Header component', () => {
   it('renders the header component', () => {
-    render(<Header />);
-    const linkElement = screen.getByRole('link', { name: /@bartigorfs/i });
-    expect(linkElement).toBeInTheDocument();
-  });
+    render(<Header />)
+    const linkElement = screen.getByRole('link', { name: /@bartigorfs/i })
+    expect(linkElement).toBeInTheDocument()
+  })
 
   it('has the correct href attribute', () => {
-    render(<Header />);
-    const linkElement = screen.getByRole('link', { name: /@bartigorfs/i });
-    expect(linkElement).toHaveAttribute('href', 'https://github.com/bartigorfs');
-  });
+    render(<Header />)
+    const linkElement = screen.getByRole('link', { name: /@bartigorfs/i })
+    expect(linkElement).toHaveAttribute('href', 'https://github.com/bartigorfs')
+  })
 
   it('opens the link in a new tab', () => {
-    render(<Header />);
-    const linkElement = screen.getByRole('link', { name: /@bartigorfs/i });
-    expect(linkElement).toHaveAttribute('target', '_blank');
-  });
+    render(<Header />)
+    const linkElement = screen.getByRole('link', { name: /@bartigorfs/i })
+    expect(linkElement).toHaveAttribute('target', '_blank')
+  })
 
   it('has the correct className', () => {
-    render(<Header />);
-    const divElement = screen.getByText(/@bartigorfs/i).closest('div');
-    expect(divElement).toHaveClass('mocked-header-class');
-  });
-});
+    render(<Header />)
+    const divElement = screen.getByText(/@bartigorfs/i).closest('div')
+    expect(divElement).toHaveClass('mocked-header-class')
+  })
+})
